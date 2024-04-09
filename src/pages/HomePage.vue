7eb0f684e127d76dcf08db3593e71a4c05b61066
Mijashh <amijash@gmail.com>
2024-04-09 10:42:36 +0545

    feat: create component HomePage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <Navbar />
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-bold">Welcome to Texplore</h1>
+        <p class="text-lg mt-4">Explore the world of Tech Gadjets</p>
+    </div>
+    <Footer />
+
+</template>
+
+<script setup>
+import Navbar from '../components/AppNavbar.vue';
+import Footer from '../components/AppFooter.vue';
+</script>
