8e2f75def9f0d1cec454cb15816a82d99102777e
Mijashh <amijash@gmail.com>
2024-04-09 10:42:11 +0545

    feat: create component for signup success

---
+++ src/components/SignupSuccess.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="max-w-md w-full bg-white shadow-md rounded-md p-6">
+      <h2 class="text-xl font-bold mb-4">Signup Successful</h2>
+      <p class="text-gray-700 mb-4">Congratulations! Your signup was successful.</p>
+      <p class="text-gray-700 mb-6">You can now login to your account.</p>
+      <button @click="redirectToLogin" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import router from '../router/routes.js';
+const redirectToLogin = () => {
+  router.push({ name: 'Login' });
+}
+
+</script>
