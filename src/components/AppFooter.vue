5b0d9e2ad08c59d05eb5e386a978452b7a22ca52
Mijashh <amijash@gmail.com>
2024-04-09 10:40:57 +0545

    feat: create component footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="bg-white text-black border border-t-black">
+        <div class="p-10 flex justify-between" >
+            <p>&copy; 2021 Texplore</p>
+            <p>Contact</p>
+        </div>
+    </footer>
+</template>
