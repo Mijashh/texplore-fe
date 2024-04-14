56e16a33c1812a8c90d9291a5ce2b779698f02a4
Mijashh <amijash@gmail.com>
2024-04-14 13:18:35 +0545

    feat: add fields name and contact

--- src/pages/UserSignUp.vue
+++ src/pages/UserSignUp.vue
@@ -4,14 +4,24 @@
             <h1 class="text-3xl font-bold mb-5">Sign Up</h1>
             <form @submit.prevent="signup">
                 <div class="mb-4">
+                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Full Name</label>
+                    <input type="text" id="name" name="name" v-model="name"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                </div>
+                <div class="mb-6">
+                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Contact Number</label>
+                    <input type="text" id="contact" name="contact" v-model="contact"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                </div>
+                <div class="mb-6">
                     <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
                     <input type="email" id="email" name="email" v-model="email"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
                 </div>
                 <div class="mb-6">
                     <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
                     <input type="password" id="password" name="password" v-model="password"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" required>
                     <p class="text-red-500 text-md italic border-red-500 " v-if="signuperror">{{ signuperror }}</p>
                 </div>
                 <div class="flex items-center justify-between">
@@ -40,10 +50,14 @@
 <script setup>
 import { ref } from 'vue';
 import signupsucess from '../components/SignupSuccess.vue';
+import {useToast} from 'vue-toastification'
 import axios from 'axios';
+const toast= useToast();
 const signuperror = ref('');
 const email = ref('');
 const password = ref(''); 
+const name = ref('');
+const contact = ref('');
 const successful = ref(false);
 
 
@@ -51,16 +65,23 @@ const signup = async () => {
     try {
         const formData = {
             email: email.value,
-            password: password.value
+            password: password.value,
+            name:name.value,
+            contact_no:contact.value
         };
         await axios.post('http://127.0.0.1:8000/users/', formData);
         successful.value = true;
-        // router.push({ name: 'Login' });
     } catch (error) {
         const responseData = error.response.data;
-        signuperror.value = responseData.error || 'An unexpected error occurred.';
+        let firstValue;
+        for (const key in responseData) {
+            if (Object.hasOwnProperty.call(responseData, key)) {
+                firstValue = responseData[key][0];
+                break; 
+            }
+        }
+        toast.error(firstValue || 'An unexpected error occurred.');
         successful.value = false;
-
     }
 };
 
