7a0f31e8254547257ea0cae5d7cae998172e8581
Mijashh <amijash@gmail.com>
2024-04-20 09:38:29 +0545

    feat: add new route to access Cart page

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import userLogin from '../pages/UserLogin.vue';
 import userSignup from '../pages/UserSignUp.vue';
 import Home from '../pages/HomePage.vue';
-
+import Cart from '../pages/UserCart.vue';
 
 const routes = [
   {
@@ -19,6 +19,11 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: userSignup
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   }
 ];
 
