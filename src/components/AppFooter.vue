0ebdd4a4b373130c788b5bd51ce99c2818454d82
Mijashh <amijash@gmail.com>
2024-04-20 09:31:52 +0545

    refactor: customize the navbar component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,19 @@
 <template>
-    <footer class="bg-white text-black border border-t-black">
-        <div class="p-10 flex justify-between" >
-            <p>&copy; 2021 Texplore</p>
-            <p>Contact</p>
+    <footer class="bg-gray-700 h-36 text-white py-4">
+      <div class="container mx-auto flex justify-between">
+        <div class="w-1/2">
+          <h3 id="contact" class="text-xl font-bold">Contact</h3>
+          <ul class="text-lg">
+            <li class="block text-gray-300 hover:text-white">Email: texplore@gmail.com</li>
+            <li class="block text-gray-300 hover:text-white">Phone: +977-XXXXXXXXX</li>
+            <li class="block text-gray-300 hover:text-white">Address: XXX Street, YYY City</li>
+          </ul>
         </div>
+        <div class="w-1/2">
+          <h3 id="about" class="text-xl font-bold">About Us</h3>
+          <p class="text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
     </footer>
-</template>
+  </template>
+  
