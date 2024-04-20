3db19854621cf02859143d37b694c412d748e879
Mijashh <amijash@gmail.com>
2024-04-20 09:37:13 +0545

    feat: include Products component  in homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,11 @@
 <template>
     <Navbar />
-    <div class="flex flex-col items-center justify-center h-screen">
-        <h1 class="text-4xl font-bold">Welcome to Texplore</h1>
-        <p class="text-lg mt-4">Explore the world of Tech Gadjets</p>
-    </div>
+    <Products />
     <Footer />
-
 </template>
 
 <script setup>
 import Navbar from '../components/AppNavbar.vue';
 import Footer from '../components/AppFooter.vue';
+import Products from '../components/ProductList.vue';
 </script>
